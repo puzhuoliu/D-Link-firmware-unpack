618b4ca644765d0967619cf53a773b42ab54996a
I4N_lpz <43575651+liupuz@users.noreply.github.com>
2022-11-10 17:22:18 +0800

    Update encrpted_de.sh

--- encrpted_de.sh
+++ encrpted_de.sh
@@ -11,7 +11,7 @@
   for ((ITERATION=0; ITERATION<ROOF; ITERATION++)); do
     (( OFFSET=131072*ITERATION ))
     dd if="$TMP_IMAGE_FILE" skip="$OFFSET" iflag=skip_bytes count=256| openssl aes-256-cbc -d -in /dev/stdin  -out /dev/stdout \
-    -K "6865392d342b4d212964363d6d7e7765312c7132613364316e26322a5a5e2538" -iv "4a253169516c38243d6c6d2d3b384145" --nopad \
+    -K "XXX" -iv "XXX" --nopad \
     --nosalt | dd if=/dev/stdin of="$EXTRACTION_FILE_" oflag=append conv=notrunc 2>&1 | tee -a "$LOG_FILE"
   done
 # Now it should be a .ubi file thats somewhat readable and extractable via ubireader
